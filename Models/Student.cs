092b3d12b32610a7b6a9b6b94468400c6642dcdb
yehonatan <yehonatan.baruchson@gmail.com>
2022-11-28 21:03:05 +0200

    crud

--- Models/Student.cs
+++ Models/Student.cs
@@ -5,19 +5,26 @@ public class Student
 {
     public int Id{get; set;}
     public String? Password{get; set;}
-    public String? Email
-    {get 
-    {
-        return Email;
-    } 
-    set
-    {
-        Email = value;
-    }
-    }
+    public String? Email{get; set;}
     public String? Name{get; set;}
     public String? Phone{get; set;}
     
+    public Student(string phone, string name, string pass, string email, int id){
+        this.Email = email;
+        this.Id = id;
+        this.Name = name;
+        this.Password = pass;
+        this.Phone = phone;
+    }
+
+    public Student(){
+        this.Email = "";
+        this.Id = -1;
+        this.Name = "";
+        this.Password = "";
+        this.Phone = "";
+    }
+
 }
 
 
--- Program.cs
+++ Program.cs
@@ -1,25 +1,68 @@
-var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+using Google.Cloud.Firestore;
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+using TiktikHttpServer.Models;
+using TiktikHttpServer.Database;
 
-var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+
+
+// var builder = WebApplication.CreateBuilder(args);
+
+// // Add services to the container.
+
+// builder.Services.AddControllers();
+// // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+// builder.Services.AddEndpointsApiExplorer();
+// builder.Services.AddSwaggerGen();
+
+// var app = builder.Build();
+
+// // Configure the HTTP request pipeline.
+// if (app.Environment.IsDevelopment())
+// {
+//     app.UseSwagger();
+//     app.UseSwaggerUI();
+// }
+
+// // app.UseHttpsRedirection();
+
+// app.UseAuthorization();
+
+// app.MapControllers();
+
+// app.Run();
+
+
+
+/*
+using System.Net.Http.Headers;
+
+using HttpClient client = new();
+// client.DefaultRequestHeaders.Accept.Clear();
+// client.DefaultRequestHeaders.Accept.Add(
+//     new MediaTypeWithQualityHeaderValue("application/vnd.github.v3+json"));
+// client.DefaultRequestHeaders.Add("User-Agent", ".NET Foundation Repository Reporter");
+
+await ProcessRepositoriesAsync(client);
+
+static async Task ProcessRepositoriesAsync(HttpClient client)
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    // var json = await client.GetStringAsync(
+    //      "https://api.github.com/orgs/dotnet/repos");
+    // var json = await client.GetStringAsync(
+    //      "https://localhost:7191/Student");
+    
+    var json = await client.GetStringAsync(
+         "https://firestore.googleapis.com/v1/projects/example-83225/databases/(default)/documents/Student");
+     Console.Write(json);
 }
 
-// app.UseHttpsRedirection();
+*/
 
-app.UseAuthorization();
+CRUD DB = new CRUD();
 
-app.MapControllers();
+DB.add_new_student("0506541482", "sharon", "123456", "sharon.shab@gmail.com", 13);
 
-app.Run();
+
+
